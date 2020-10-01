c621b2a637f443f06ecc1af1c42303f8946b664e
Baldi's Basics Unofficial Account <45296094+WhatAreThose@users.noreply.github.com>
2020-10-01 15:49:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,14 +16,18 @@ client.on('message', message => {
 
     if (message.content === 'Hi') {
 
-       message.channel.send('Hello! \n(Custom Bot By <@!430425679834644491>)');
+       message.channel.send('Hello! \nI am a custom bot made by <@!430425679834644491>. You can DM him if you want to know how to make a bot! He will simplify the steps if you have not made one before.');
 
        }
 
+    if (message.content === 'mb!steal') {
+     
+     message.channel.send('Why are you trying to steal me? :(')
+     sleep(2)
+     message.channel.send('Ok fine... here is the link to take me: https://github.com/WhatAreThose/DiscordBotLol/')
+     
+    }
 });
 
  
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN should be under Bot > Token.
