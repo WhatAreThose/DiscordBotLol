4d0d59fd38fceee0f93acbc8c9f54579fb36a6d9
Baldi's Basics Unofficial Account <45296094+WhatAreThose@users.noreply.github.com>
2020-10-02 09:49:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.author.bot) return;
+    if (message.author.bot) return;
 
     if (message.content === 'Hi') {
        message.channel.send('Hello! \nI am a custom bot made by <@!430425679834644491>. You can DM him if you want to know how to make a bot! He will simplify the steps if you have not made one before.');
